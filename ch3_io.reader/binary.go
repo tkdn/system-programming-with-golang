5b295f56fd3d4f9a6f7136816c979003f14ba6a6
tkdn <tkdnation@gmail.com>
2024-03-30 22:10:41 +0900

    バイナリ解析 PNGファイルにテキストチャンクを追加

--- ch3_io.reader/binary.go
+++ ch3_io.reader/binary.go
@@ -1,8 +1,10 @@
 package ch3ioreader
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"os"
 )
@@ -34,6 +36,19 @@ func dumpChunk(chunk io.Reader) {
 	fmt.Printf("chunk %q (%d bytes)\n", string(buff), length)
 }
 
+func textChunk(text string) io.Reader {
+	byteData := []byte(text)
+	var buff bytes.Buffer
+	binary.Write(&buff, binary.BigEndian, int32(len(byteData)))
+	buff.WriteString("tEXt")
+	buff.Write(byteData)
+	// CRC を計算し追加
+	crc := crc32.NewIEEE()
+	io.WriteString(crc, "tEXt")
+	binary.Write(&buff, binary.BigEndian, crc.Sum32())
+	return &buff
+}
+
 func ReadPng(path string) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -43,5 +58,28 @@ func ReadPng(path string) {
 	chunks := readChunk(file)
 	for _, chunk := range chunks {
 		dumpChunk(chunk)
+	}
+}
+
+func AddTextChunk(src string, dist string) {
+	file, err := os.Open(src)
+	if err != nil {
+		fmt.Printf("can't open file: %v", err)
+	}
+	defer file.Close()
+	newFile, err := os.Create(dist)
+	if err != nil {
+		fmt.Printf("can't create file: %v", err)
+	}
+	defer newFile.Close()
+	chunks := readChunk(file)
+	// シグニチャ書き込み
+	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+	// 先頭に必要な IHDR チャンク書き込み
+	io.Copy(newFile, chunks[0])
+	// テキストチャンク追加
+	io.Copy(newFile, textChunk("FooBar2000"))
+	for _, chunk := range chunks {
+		io.Copy(newFile, chunk)
 	}
 }
