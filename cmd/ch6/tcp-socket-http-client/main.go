d0bacab3d4be2fddbcf1d1800a52379ea53e2b39
tkdn <tkdnation@gmail.com>
2024-04-07 15:35:49 +0900

    ch6 TCPソケットを使ったHTTPクライアント、keep-alive 対応

--- cmd/ch6/tcp-socket-http-client/main.go
+++ cmd/ch6/tcp-socket-http-client/main.go
@@ -6,27 +6,54 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 // see: https://github.com/tkdn/system-programming-with-golang/blob/c4cabe08f00ec3dc8f04b26e35524a171dd4ee99/ch3_io.reader/conn.go#L12
 // User-Agent: Go-http-client/1.1
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		panic(err)
+	sendMessages := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
-	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
-	if err != nil {
-		panic(err)
+	current := 0
+	var conn net.Conn = nil
+	for {
+		var err error
+		// 接続がまだなければ開始
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:8080")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+		// POST で文字列を送信
+		req, err := http.NewRequest("POST", "http://localhost:8080", strings.NewReader(sendMessages[current]))
+		if err != nil {
+			panic(err)
+		}
+		err = req.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+		// サーバから読み込み、タイムアウトはここでエラーとなるのでリトライする
+		res, err := http.ReadResponse(bufio.NewReader(conn), req)
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+		dump, err := httputil.DumpResponse(res, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+		current++
+		if current == len(sendMessages) {
+			break
+		}
 	}
-	req.Write(conn)
-	res, err := http.ReadResponse(bufio.NewReader(conn), req)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(res, true)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
+	conn.Close()
 }
