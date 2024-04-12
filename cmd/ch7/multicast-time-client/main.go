40b3b38c1d07e5bdb49853940c1ba4a9080963d4
tkdn <tkdnation@gmail.com>
2024-04-13 07:02:31 +0900

    UDPソケットを使ったtickサーバ・受信クライアント

---
+++ cmd/ch7/multicast-time-client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Listen tick server at 224.0.0.1:9999")
+	address, err := net.ResolveUDPAddr("udp", "224.0.0.1:9999")
+	if err != nil {
+		panic(err)
+	}
+	listener, err := net.ListenMulticastUDP("udp", nil, address)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	buff := make([]byte, 1500)
+
+	for {
+		length, remoteAddress, err := listener.ReadFromUDP(buff)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Server %v\n", remoteAddress)
+		fmt.Printf("Now %s\n", string(buff[:length]))
+	}
+}
